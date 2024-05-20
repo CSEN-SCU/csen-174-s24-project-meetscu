2336e4ea1335b7a0e8716968594274f3cbf4c762
Byron Josh Cuachin <byroncuachin@gmail.com>
2024-05-19 23:48:32 -0700

    Changing app to render login screen

--- frontend/App.js
+++ frontend/App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from "react";
-import AppNavigation from './src/navigation/AppNavigation';
+// import AppNavigation from './src/navigation/AppNavigation';
 import LoginScreen from './src/screens/login';
 
 
