669c92c74f7e4629619c9585830d9fbbd78bbdda
Byron Josh Cuachin <byroncuachin@gmail.com>
2024-05-21 18:02:09 -0700

    Removed unnecessary env plugin

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -3,14 +3,6 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      [
-        'module:react-native-dotenv',
-        {
-          envName: 'APP_ENV',
-          moduleName: '@env',
-          path: '.env',
-        },
-      ],
       ['nativewind/babel'],
       [
       "module-resolver",
